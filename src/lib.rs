16ac2ac19859f6b4bcb8d95952fc2d18eeca71fa
Frank Angelo Malubag <frankabad189@gmail.com>
2026-04-09 20:29:59 +0800

    feat(configuration method & route hanlder): Created the route handler for the that would be used for the Handler registration in the configuration method having the path to /subscriptions.

--- src/lib.rs
+++ src/lib.rs
@@ -8,15 +8,23 @@ async fn greet(req: HttpRequest) -> impl Responder {
     format!("Hello {}", &name)
 }
 
-async fn health_check() -> impl Responder {
+// We were returning `impl Responder` at the very beginning.
+// We are now spelling out the type explicityly given that we have become more familiar with actix-web
+// There is no performance difference! Just a stylistic choice
+async fn health_check() -> HttpResponse {
     HttpResponse::Ok().finish()
 }
 
+// Let's start simple: we always return a 200 OK
+async fn subscribe() -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
 // pub async fn run() -> std::io::Result<()> {
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
     let server= HttpServer::new( || { 
         App::new()
             .route("/health_check", web::get().to(health_check))
+            .route("/subscriptions", web::post().to(subscribe))
             .route("/", web::get().to(greet))
             .route("/{name}", web::get().to(greet))
     })
