2f8c5420102cb5b6e5c0ca5f402d1eb702fc5671
Frank Angelo Malubag <frankabad189@gmail.com>
2026-04-13 17:28:56 +0800

    feat(running migrations): Added the sqlx migrate run which it will finds, validates, and applies pending migrations scripts to the database from reading the /migrations folder (a Migration folder that was created from sqlx migrate add create_subscriptions_table)
    it would read the migrations folder, connect to the database using DATABASE_RUL, tracks the state with _sqlx_migrations, and runs the Pending scripts from migrations/ folder, then updates the table, and insert a new row into the _sqlx_migrations to record migration in complete.

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -23,15 +23,20 @@ DB_PASSWORD="${POSTGRES_PASSWORD:=password}"
 DB_NAME="${POSTGRES_DB:=newsletter}"
 # Check if a custom port has been set, otherwise default to '5432'
 DB_PORT="${POSTGRES_PORT:=5432}"
+
+# Allow to skip Docker if a dockerized Postgres database is already running
+if [[ -z "${SKIP_DOCKER}" ]]
+then
 # Launch postgres using Docker
 docker run \
--e POSTGRES_USER=${DB_USER} \
--e POSTGRES_PASSWORD=${DB_PASSWORD} \
--e POSTGRES_DB=${DB_NAME} \
--p "${DB_PORT}":5432 \
--d postgres \
-postgres -N 1000
+  -e POSTGRES_USER=${DB_USER} \
+  -e POSTGRES_PASSWORD=${DB_PASSWORD} \
+  -e POSTGRES_DB=${DB_NAME} \
+  -p "${DB_PORT}":5432 \
+  -d postgres \
+  postgres -N 1000
 # ^ Increased maximum number of connections for testing purpose
+fi
 
 
 # Keep pinging Postgres until it's ready to accept commands
@@ -45,3 +50,6 @@ done
 
 export DATABASE_URL=postgres://${DB_USER}:${DB_PASSWORD}@localhost:${DB_PORT}/${DB_NAME}
 sqlx database create
+sqlx migrate run
+
+>&2 echo "Postgres has been migrated, ready to go!"
