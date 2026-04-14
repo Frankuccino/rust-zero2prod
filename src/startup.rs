7e9158fc78cbe45ef1c6c70a154ac8f372bfde52
Frank Angelo Malubag <frankabad189@gmail.com>
2026-04-14 11:12:11 +0800

    refactor: import route handlers into startup scope
    - Use crate::routes to access re-exported handlers
    - Clean up run function by using short names for health_check and subscribe

--- src/startup.rs
+++ src/startup.rs
@@ -1,14 +1,16 @@
 use actix_web::{App, HttpServer, web};
 use actix_web::dev::Server;
 use std::net::TcpListener;
+
+// Bring the re-exported routes into scope
+use crate::routes::{health_check, subscribe};
+
 // Startup will host the run function
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
     let server= HttpServer::new( || { 
         App::new()
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
-            .route("/", web::get().to(greet))
-            .route("/{name}", web::get().to(greet))
     })
     // .bind(("127.0.0.1", 8000))?
     .listen(listener)?
