fe3210ddbe5a5f4a12ef52de638c7fb341a99952
Frank Angelo Malubag <frankabad189@gmail.com>
2026-04-17 19:28:55 +0800

    modified the main function to readthe configuration that was recently added as a feature to pull the application's configuration port and listen to that address

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
 use zero2prod::startup::run;
 use std::net::TcpListener;
+use zero2prod::configuration::get_configuration;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    // Bubble up the io::Error if we failed to bind the address 
-    // Otherwise call .await on our Server
-    let listener = TcpListener::bind("127.0.0.1:8000")?;
+    // Public if we can't read configuration
+    let configuration = get_configuration().expect("Failed to read configuration");
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+
+    // We have removed the hard-coded "8000" - it's now coming from out settings!
+    let listener = TcpListener::bind(address)?;
     run(listener)?.await
 }
