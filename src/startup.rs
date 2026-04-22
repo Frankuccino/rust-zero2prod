cffabfc6c852975b76963060d9a1c115abae2c37
Frank Angelo Malubag <frankabad189@gmail.com>
2026-04-22 22:33:25 +0800

    feat(PgConnection): Inject database connection into the server startup
    - Update run signature to accept PgConection - Registered the connection as shared data using web::Data- Register the connection using .app_data in the server factory so it can be cloned across worker threads.

--- src/startup.rs
+++ src/startup.rs
@@ -1,16 +1,21 @@
 use actix_web::{App, HttpServer, web};
 use actix_web::dev::Server;
 use std::net::TcpListener;
+use sqlx::PgConnection;
 
 // Bring the re-exported routes into scope
 use crate::routes::{health_check, subscribe};
 
 // Startup will host the run function
-pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server= HttpServer::new( || { 
+pub fn run(listener: TcpListener, connection: PgConnection) -> Result<Server, std::io::Error> {
+    // Wrap the connection in a smart pointer
+    let connection = web::Data::new(connection);
+
+    let server= HttpServer::new( move || { 
         App::new()
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
+            .app_data(connection.clone())
     })
     // .bind(("127.0.0.1", 8000))?
     .listen(listener)?
