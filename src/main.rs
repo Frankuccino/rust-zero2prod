8d3dd4028a0e222802ac234f3aef1c262e1cce55
Frank Angelo Malubag <frankabad189@gmail.com>
2026-04-22 22:35:56 +0800

    feat: update main and subscriptions to use shared database connection.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use sqlx::{Connection, PgConnection};
 use zero2prod::startup::run;
 use std::net::TcpListener;
 use zero2prod::configuration::get_configuration;
@@ -6,9 +7,13 @@ use zero2prod::configuration::get_configuration;
 async fn main() -> std::io::Result<()> {
     // Public if we can't read configuration
     let configuration = get_configuration().expect("Failed to read configuration");
+    let connection = PgConnection::connect(&configuration.database.connection_string())
+        .await
+        .expect("Failed to connect to Postgres.");
+
     let address = format!("127.0.0.1:{}", configuration.application_port);
 
     // We have removed the hard-coded "8000" - it's now coming from out settings!
     let listener = TcpListener::bind(address)?;
-    run(listener)?.await
+    run(listener, connection)?.await
 }
--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -6,6 +6,7 @@ pub struct FormData {
     name: String
 }
 
+// Let's start simple: we always return a 200 OK
 pub async fn subscribe(_form: web::Form<FormData>) -> HttpResponse {
     HttpResponse::Ok().finish()
     // format!("Welcome {}", form.username)
