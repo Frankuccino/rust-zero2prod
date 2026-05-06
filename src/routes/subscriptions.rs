e6c90dabff4de4b2562b63839fdd85e31678964b
Frank Angelo Malubag <frankabad189@gmail.com>
2026-05-06 20:09:27 +0800

    feat(uuid): Added a request_id as an uuidv4 to determine the log discrepancies on concurrent log operations

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -15,13 +15,17 @@ pub async fn subscribe(
     form: web::Form<FormData>,
     pool: web::Data<PgPool>) 
     -> HttpResponse {
-    
+    let request_id = Uuid::new_v4();
     log::info!(
-        "Adding '{}' '{}' as a new subscriber",
+        "request_id {}- Adding '{}' '{}' as a new subscriber",
+        request_id,
         form.email,
         form.name
     );
-    log::info!("Saving new subscriber details in the database");
+    log::info!(
+        "request_id {} - Saving new subscriber details in the database", 
+        request_id
+    );
     match sqlx::query!(
         r#"
         INSERT INTO subscriptions (id, email, name, subscribed_at)
@@ -36,10 +40,18 @@ pub async fn subscribe(
     .await
     {
         Ok(_) => {
-            log::info!("New subscriber details have been saved");
+            log::info!(
+                "request_id {} - New subscriber details have been saved",
+                request_id
+            );
              HttpResponse::Ok().finish()
          },
         Err(e) => {
+            log::error!(
+                "request_id {} - Failed to execute query {:?}",
+                request_id,
+                e
+            );
             println!("Failed to execute query: {}", e);
             HttpResponse::InternalServerError().finish()
         }
